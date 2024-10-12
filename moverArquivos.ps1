01500402523e125dfb61ddc19e3821cd13d35d6c
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2024-10-12 18:26:41 -0300

    Update moverArquivos.ps1

--- moverArquivos.ps1
+++ moverArquivos.ps1
@@ -1,82 +1,44 @@
-# esse script tem o intuito de enviar os arquivos criados no desktop do usuário para suas respectivas pastas
-# Para que o usuário nem precise fazer isso manualmente
-
-$homeUsuario = "$HOME/Desktop"
+# lista de programas a serem instalados
+$listaDeProgramas = @(
+    'Google.Chrome',
+    'obs.studio',
+    'cockos.reaper',
+    'flameshot'
+    'Valve.Steam',
+    'anydesk',
+    'python3',
+    'sublime',
+    'mpv',
+    'KDE.Kate',
+    'wsl',
+    'libreoffice',
+    'appmakes.Typora',
+    'vscode'
+)
 $plataforma = $PSEdition
 
-# criar uma função caso os diretorios não existam, cria-los
-Function criarDiretorio(){
-    param(
-        $pasta
-    )
-    # Se não existir o diretório crie
-    if (-not(Test-Path $pasta)){
-        Write-Host -ForegroundColor red "Sua pasta não existe $pasta"; Start-Sleep -Seconds 5
-        New-Item -Type Directory -Path $pasta
-        Write-Host -ForegroundColor red "Criado com sucesso $pasta"
+#Write-Host ($listaDeProgramas).Langht
+# for para percorrer  a lista de instlar os programas
+foreach ($programa in $listaDeProgramas){
+    if( $programa -match 'sublime' ){
+        write-Host Instalndo o $programa.ToUpper() via WGET -ForegroundColor red
+        Start-Sleep -Seconds 2
+        if ($plataforma.Tolower() -eq 'desktop') {
+            wget 'https://download.sublimetext.com/Sublime%20Text%20Build%203211%20x64%20Setup.exe'  -O $HOME/Desktop/sublime.exe
+            Start-Process $HOME/Desktop/sublime.exe
+        }
+        else{
+            sudo apt install sublime-text
+        }
     }
-}
-
-
-# criar lista de diretorios para enviar os arquivos correspondentes
-$diretorios = @(
-    'py',
-    'sh',
-    'awk',
-    'csv',
-    'markdown',
-    'powershell',
-    'vbs'
-    #'testePowershell'
-)
-
-
-# Criar um lista de tipos de arquivos
-
-$arquivos = @(
-    'py',
-    'sh',
-    'awk',
-    'csv',
-    'md',
-    'ps1',
-    'vbs'
-)
-
-
-for ($i = 0;$i -lt $diretorios.Count;$i++){
-    #Get-Item  "$HOME/Área de Trabalho/$chave[$i]/"
-    #echo $diretorios[$i]
-    #echo $arquivos[$i]
-    if ($plataforma.ToLower() -eq 'desktop'){
-
-        $diretorioAtual = Join-Path -Path "$HOME/Desktop/" -ChildPath $diretorios[$i]
-        $diretorioAtual
-        #start-sleep -Seconds 5
-        criarDiretorio $diretorioAtual
-        $arquivoAtual = $arquivos[$i]
-        Write-Host $arquivos[$i] -ForegroundColor green
-        $d = (Get-ChildItem -Path "$homeUsuario\*.$arquivoAtual")
-        Write-Host "$d" -ForegroundColor red
-        $d.Name | Move-Item -Destination $diretorioAtual #-Confirm
-        #$d = Join-Path $d -ChildPath *.$($arquivos[$i])
-        #$d
-        start-sleep -Seconds 5
-
-        #Move-Item "$homeUsuario/*" $diretorioAtual -Confirm
-        #(Get-Item $diretorioAtual/*).Name
-    }else{
-
-        $diretorioAtual = Join-Path -Path "$HOME/$env:areaDeTrabalhoUsuario" -ChildPath $diretorios[$i]
-        #$diretorioAtual | out-file -FilePath '~/Área de Trabalho/atatatata.txt'
-        #start-sleep -Seconds 5
-        criarDiretorio $diretorioAtual
-        $arquivoAtual = $arquivos[$i]
-        $d = (Get-ChildItem $HOME/"$env:areaDeTrabalhoUsuario"/*.$arquivoAtual) | Move-Item -Destination $diretorioAtual
-        $d.name | out-file -FilePath '~/Área de Trabalho/atatatata.txt' -Append
-        start-sleep -Seconds 5
-
-    echo '' ''
+    #Write-Host "$programa" -ForegroundColor Cyan
+    if ($programa -eq 'wsl'){ wsl --install --distribution Debian }
+    try {
+        Start-Process winget.exe -ArgumentList 'install',$programa -NoNewWindow -wait
+        Write-Host "$programa instalado ou atualizado com sucesso" -ForegroundColor Cyan
+        Start-Sleep -Seconds 1
     }
-
+    catch{
+        Write-Host "Erro fatal" -ForegroundColor Cyan
+    }
 }
