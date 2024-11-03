35f68efac89ade67cd489a1610e36f4aee9b206f
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2024-11-03 10:33:32 -0300

    Add files via upload

---
+++ bibliotecas/nico.ps1
@@ -0,0 +1,17 @@
+$nico = @(
+    '7hLdWJgObro',
+    'UomVN1kJ12o',
+    'g-oud7oplUo',
+    'XwjVSjLGrig',
+    'Z6JjH3dyY1U',
+    'kHF_8KvS-xI',
+    '4j5eB3L90Qw',
+    '0qffup1UA0A',
+    '7apOQIpUN7o',
+    'uTUA6k9HZ-4',
+    'GX7HZcofOJA',
+    'dj2DZLLUWrs',
+    'cXf3xdnSdSA',
+    'GJsvzSIdt3Y'
+)
+
