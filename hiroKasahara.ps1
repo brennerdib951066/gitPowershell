1bf95c7ac3660d3f185cce9993a81e59ce847554
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2024-11-08 18:41:19 -0300

    Create hiroKasahara.ps1

---
+++ hiroKasahara.ps1
@@ -0,0 +1,12 @@
+$renaiseiriron = @(
+            'lFfwVD8DuBI',
+            'M8KJdaFwCxs',
+            'HTK6gCTTCpY',
+            'o3tV57ZxSgw',
+            'XhCfJFWbX-U',
+            'dGzjT5NJkV4',
+            '2anr5y0Bi_s',
+            'h7TYbOZMHYA',
+            'jFvftiZqNeA',
+            'IQsqWgEGLa8'
+)
