8a97512abc10aacbc678e1ccdcf477b791930196
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2024-11-20 10:51:40 -0300

    Add files via upload

---
+++ abriBubble.ps1
@@ -0,0 +1,39 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 72dbb491-8a24-4ad7-84a7-5692e782c585
+
+.AUTHOR brenner
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ abrir flameshot 
+
+#> 
+chrome --profile-directory='DIB' 'https://bubble.io/page?id=viverbemseguroscrm&tab=Design&name=escolhaLocomocao&type=custom&version=41h0i' 'https://bubble.io/page?id=viverbemseguroscrm&tab=Data&name=escolhaLocomocao&type=custom&subtab=Data+Types&type_id=outros_dados&version=41h0i' 'https://www.sistemaviverbemseguros.com/version-41h0i'
