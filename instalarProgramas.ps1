e6a18fc29676a05797c493937884c7c3b7e425a7
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2024-11-29 15:21:00 -0300

    Create instalarProgramas.ps1

---
+++ instalarProgramas.ps1
@@ -0,0 +1,59 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 55cce987-6f94-448e-b02d-7bec779e9a8b
+
+.AUTHOR brenner
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ instalar programas via winget ou via apt do linux
+
+#> 
+$listaDeProgramas = @(
+    'typora',                 # TYPORA
+    'flameshot',
+    'reaper',                 # Reaper
+    'XP8BV7F2CT4LDK',               # GOOGLE CHROME
+    'XPFFH613W8V6LV',               # OBS STUDIO
+    'XP8CD3D3Q50MS2',               # PEEK
+    'AnyDeskSoftwareGmbH.AnyDesk',  # ANYDESK
+    'libreoffice',
+    'Valve.Stean'
+)
+$plataforma = $PSEdition
+
+ForEach ($programaAtual in $listaDeProgramas){
+    if (-not($plataforma -match 'desktop')){
+        sudo apt install $programaAtual
+    }
+    winget install $programaAtual
+}
+
+
