a09e034a90342e674e2e559641b66be01521f0a7
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-02-28 07:46:45 -0300

    Update notificarWhatsApp.ps1

--- notificarWhatsApp.ps1
+++ notificarWhatsApp.ps1
@@ -15,6 +15,29 @@ Function notificarWhatsApp {
         $mensagem,
         $idSubcriber
     )
+    # Criando uma verificação com switch case para ver o segundo argumento passado
+    if ($mensagem) {
+        Switch ("$idSubcriber".ToLower()) {
+            'brenner' {
+                #$args[2] = '385910829'
+                Write-Host -ForegroundColor DarkYellow "digitou $idSubcriber".ToUpper()
+                $idSubcriber = '385910829' # BRENNER
+                #Exit
+            }
+            'denner' {
+                Write-Host -ForegroundColor DarkYellow "digitou $idSubcriber".ToUpper()
+                $idSubcriber = '193741501' # DENNER
+                #Exit
+            }
+            'daniele' {
+                Write-Host -ForegroundColor DarkYellow "digitou $idSubcriber".ToUpper()
+                $idSubcriber = '193744989' # DANIELE
+                #Exit
+            }
+        } # SWITCH CASE
+        #Write-Host -ForegroundColor DarkYellow "digitou $($args[3])".ToUpper()
+    } # IF PARA O SWITCH CASE
+
     $cabecalho = @{
         "Content-Type" = "application/json"
         "API-KEY" = "c7e572f0-c17b-4304-9478-b68641234d6c"
@@ -42,4 +65,3 @@ if ($args[0]){
     }
     Write-Host -ForegroundColor Yellow "Você tem esse argumento $($args[0])"
 }
-#notificarWhatsApp 'aqui é um teste com powershell no DEBIAN'.Tolower() '385910829'
