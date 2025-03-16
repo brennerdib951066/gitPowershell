3edd3a377708ee64ab91232c4a72fd15bd86fb13
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-03-16 11:12:29 -0300

    Update criarPath.ps1

--- criarPath.ps1
+++ criarPath.ps1
@@ -39,21 +39,28 @@
 
 # Lsita de programas a serem adicionados no path se não existirem
 $programas = @(
-    'C:\Program Files\Google\Chrome\Application\',
-    'C:\Program Files\Flameshot\bin\',
-    'C:\Program Files\obs-studio\bin\64bit\',
-    "$env:USERPROFILE\AppData\Local\Programs\Peek\",
-    'C:\Program Files\LibreOffice\program\',
+    'C:\Program Files\Google\Chrome\Application',
+    'C:\Program Files\Flameshot\bin',
+    'C:\Program Files\obs-studio\bin\64bit',
+    'C:\Users\brenner\AppData\Local\Programs\Peek',
+    'C:\Program Files\LibreOffice\program',
     "$env:USERPROFILE\Desktop\powershell",
     "C:\Program Files (x86)\AnyDesk",
-    "C:\Program Files\REAPER (x64)\"
+    "C:\Program Files\REAPER (x64)"
+    'C:\Program Files\Barrier',
+    'C:\Users\brenner\AppData\Local\Programs\Kate\bin',
+    'C:\Users\brenner\Downloads\mpv-x86_64-20241125-git-86bbe99',
+    'C:\Users\brenner\AppData\Local\Microsoft\WindowsApps',
+    'C:\Users\brenner\AppData\Local\Programs\Typora'
 )
 
 ForEach ($programa in $programas){
-    if (-not (($env:Path).Split(';') | Where-Object {$_ -like "$programa"})) {
+    if (-not (($env:Path).Split(';') -contains "$programa" )) {
         Write-Host -ForegroundColor green $programa
         Start-Sleep -Seconds 5
-        [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$programa", [System.EnvironmentVariableTarget]::User)
+	$newPath = [System.Environment]::GetEnvironmentVariable('PATH', 'User') + ";$programa"
+	[System.Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
+        #[System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$programa", [System.EnvironmentVariableTarget]::User)
     } # If para verificar se não exitir no diretorio no path, adiciona-lo
 
 } # For para percorrer a lista de programa
