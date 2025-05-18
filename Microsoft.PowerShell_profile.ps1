c35cc5cb8a147b0e11d67db810eb4e8d8764e173
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-05-18 07:56:13 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -816,7 +816,7 @@ Function criarHtml {
     </main>
 </body>
 </html>
-		@' | Out-File -FilePath "$AreaDeTrabalhoUsuario/$criarArquivo"
+		@' | Out-File -FilePath "$AreaDeTrabalhoUsuario/$criarArquivo" -Encoding UTF8
 
 		code "$AreaDeTrabalhoUsuario/$criarArquivo" | Out-Null
 	}
