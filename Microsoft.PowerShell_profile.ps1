214ed19ea63279f2ae6bdc35e525416f3b466ef0
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-05-19 06:33:12 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -823,14 +823,14 @@ Function criarHtml {
     </main>
 </body>
 </html>
-'@ | Out-File -FilePath "$AreaDeTrabalhoUsuario/$criarArquivo" -Encoding UTF8
+'@ | Out-File -FilePath `"$AreaDeTrabalhoUsuario/$criarArquivo`" -Encoding UTF8
 
 	try {
 		$programaIDE = 'code'.ToLower()
 		(Get-Command "$programaIDE" -ErrorAction stop | Out-Null).Source
 		Write-Host -ForegroundColor Green "Abrindo seu arquivo $AreaDeTrabalhoUsuario/$criarArquivo"
 		Start-Sleep -Seconds 5s
-		code "$AreaDeTrabalhoUsuario/$criarArquivo" | Out-Null
+		code "$AreaDeTrabalhoUsuario/$criarArquivo"
 	}
 	Catch {
 		Write-Host -ForegroundColor Red "Instale o visual estudio para prosseguir"
