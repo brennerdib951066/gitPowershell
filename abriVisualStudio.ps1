f49ccf36a455ae799af11e7b1e5ce3f4607b1e0a
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-06-03 07:04:29 -0300

    Add files via upload

---
+++ abriVisualStudio.ps1
@@ -0,0 +1,45 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 72dbb491-8a24-4ad7-84a7-5692e782c585
+
+.AUTHOR brenner
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ abrir flameshot 
+
+#> 
+if ( -not (Get-Command code -ErrorAction Ignore)) {
+    Exit
+}
+
+Code
+
+
