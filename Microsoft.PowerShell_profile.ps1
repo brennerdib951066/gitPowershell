0665707445a7032a683edb586ac8dda73028d5f5
brennerdib951066 <110617531+brennerdib951066@users.noreply.github.com>
2025-06-19 05:47:53 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -793,8 +793,13 @@ else {
 	$arquivoPs1 = 'Microsoft.PowerShell_profile.ps1'
 	# Configurando para que o powershell ignorar o case dos diretorios
 	$pastaDestino = (Get-ChildItem "$HOME" -Filter "Área de Trabalho" -Directory | Where-Object { $_.Name -ieq "Área de Trabalho" }).FullName
-	Invoke-WebRequest "https://raw.githubusercontent.com/brennerdib951066/gitpowershell/refs/heads/main/$arquivoPs1" -OutFile "$pastaDestino/powershell/$arquivoPs1"
-	Copy-Item "$pastaDestino/powershell/$arquivoPs1" "$PROFILE"
+	Try {
+		Invoke-WebRequest "https://raw.githubusercontent.com/brennerdib951066/gitpowershell/refs/heads/main/$arquivoPs1" -OutFile "$pastaDestino/powershell/$arquivoPs1" -ErrorAction Stop
+		Copy-Item "$pastaDestino/powershell/$arquivoPs1" "$PROFILE"
+	}
+	Catch {
+
+	}
 }
 
 <# if (verificandoPlataforma){
